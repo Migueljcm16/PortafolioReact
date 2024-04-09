67c71574533965964f1cb66b882793176d2cb34b
Migueljcm16 <31miguelop31@gmail.com>
2024-04-09 17:04:56 -0500

    Update 1704

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 
 import Main from './components/paginas/main/Main'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Main/>
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Main/>
   </React.StrictMode>,
 )
